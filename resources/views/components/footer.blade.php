424f65766aeb7ded8e7f694bc9c958a4c0bde915
TessyJr <christopher.nathanael1217@gmail.com>
2023-10-11 14:39:32 +0700

    style: fix footer stylings

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -20,7 +20,7 @@
                 <h1 class="font-extrabold text-base sm:text-lg md:text-xl">Contact Us</h1>
             </div>
 
-            <div class="flex gap-4">
+            <div class="flex items-center gap-4">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-8 aspect-square flex-none" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
                 </svg>
@@ -28,14 +28,14 @@
                 <p class="text-custom-white/70">Jl. Utan Jati Ruko Daan Mogot Baru Blok LB 2 No. 15, Kec. Kalideres, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11830</p>
             </div>
 
-            <div class="flex gap-4">
+            <div class="flex items-center gap-4">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-8 aspect-square" fill="currentColor" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
                 </svg>
                 <p class="text-custom-white/70">(021) 29018868</p>
             </div>
 
-            <div class="flex gap-4">
+            <div class="flex items-center gap-4">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-8 aspect-square" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
                 </svg>
