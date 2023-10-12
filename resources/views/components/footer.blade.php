761f0f50097e066f19120a0fbd683ca3040a36e8
TessyJr <christopher.nathanael1217@gmail.com>
2023-10-12 14:46:17 +0700

    style: fix navbar, footer, project stylings

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="text-paragraph bg-custom-darker-blue text-custom-white c-container py-16">
+<footer class="text-paragraph bg-custom-darker-blue text-custom-white c-container py-12 sm:py-14 md:py-16">
     <div class="grid gap-16 grid-cols-1 xl:grid-cols-9">
         <div class="flex flex-col gap-4 col-span-1 xl:col-span-3">
             <div class="flex justify-center sm:justify-start mb-4">
@@ -113,20 +113,20 @@
                 <div class="flex flex-col gap-4">
                     <h1 class="md:text-end text-base sm:text-lg md:text-xl font-semibold">FOLLOW US</h1>
 
-                    <div class="text-custom-darker-blue flex gap-4 sm:gap-6 md:gap-8">
-                        <a href="" target="_blank" rel="noopener noreferrer" class="bg-custom-white w-12 sm:w-14 md:w-16 aspect-square p-3 flex justify-center items-center rounded-full">
+                    <div class="text-custom-darker-blue flex gap-4 sm:gap-6 md:gap-7 lg:gap-8">
+                        <a href="" target="_blank" rel="noopener noreferrer" class="bg-custom-white w-12 sm:w-14 md:w-[3.75rem] lg:w-16 aspect-square p-3 md:p-3.5 flex justify-center items-center rounded-full">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-full aspect-square" fill="currentColor" viewBox="0 0 16 16">
                                 <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
                               </svg>
                         </a>
 
-                        <a href="" target="_blank" rel="noopener noreferrer" class="bg-custom-white w-12 sm:w-14 md:w-16 aspect-square p-3 flex justify-center items-center rounded-full">
+                        <a href="" target="_blank" rel="noopener noreferrer" class="bg-custom-white w-12 sm:w-14 md:w-[3.75rem] lg:w-16 aspect-square p-3 md:p-3.5 flex justify-center items-center rounded-full">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-full aspect-square" fill="currentColor" viewBox="0 0 16 16">
                                 <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z"/>
                               </svg>
                         </a>
 
-                        <a href="" target="_blank" rel="noopener noreferrer" class="bg-custom-white w-12 sm:w-14 md:w-16 aspect-square p-3 flex justify-center items-center rounded-full">
+                        <a href="" target="_blank" rel="noopener noreferrer" class="bg-custom-white w-12 sm:w-14 md:w-[3.75rem] lg:w-16 aspect-square p-3 md:p-3.5 flex justify-center items-center rounded-full">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-full aspect-square" fill="currentColor" viewBox="0 0 16 16">
                                 <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                               </svg>
@@ -136,7 +136,7 @@
                 </div>
 
                 <div>
-                    <img src="{{ asset('assets/logo/manusa.png') }}?t={{ env('VERSION_TIME') }}" alt="">
+                    <img src="{{ asset('assets/logo/manusa.png') }}?t={{ env('VERSION_TIME') }}" alt="manusa" class="w-40 sm:w-48 md:w-auto">
                 </div>
             </div>
         </div>
--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -13,181 +13,181 @@
     </div>
 
     <div class="c-container text-paragraph py-16 sm:py-24 md:py-32 flex flex-col gap-8">
-        <div class="bg-custom-lighter-blue px-8 pt-6 pb-3 relative">
-            <h1 class="text-custom-dark-blue font-ttRamillas font-extrabold absolute -top-5 text-2xl sm:text-3xl md:text-4xl">Photos</h1>
+        <div class="bg-custom-lighter-blue px-6 sm:px-7 md:px-8 pt-4 sm:pt-5 md:pt-6 pb-2 sm:pb-2.5 md:pb-3 relative">
+            <h1 class="text-custom-dark-blue font-ttRamillas font-extrabold absolute -top-4 sm:-top-[1.125rem] md:-top-5 text-2xl sm:text-3xl md:text-4xl">Photos</h1>
             <p class="text-custom-dark-blue/90">Beberapa foto dari hasil proyek Automatic Door yang telah kami kerjakan</p>
         </div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 text-center">
-            <div class="flex flex-col gap-4 items-center col-span-1">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-10 md:gap-12 text-center">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/1.jpg')}}" alt="Mall E Walk (North Entrance) - Balikpapan" class="w-full">
                 <p class="font-bold">Mall E Walk (North Entrance) - Balikpapan</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/2.jpg')}}" alt="Mall E Walk (East Entrance) - Balikpapan" class="w-full">
                 <p class="font-bold">Mall E Walk (East Entrance) - Balikpapan</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/3.jpg')}}" alt="Mall Senayan City - Jakarta" class="w-full">
                 <p class="font-bold">Mall Senayan City - Jakarta</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/4.jpg')}}" alt="Mall E Walk (North Entrance) - Balikpapan" class="w-full">
                 <p class="font-bold">Mall E Walk (North Entrance) - Balikpapan</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/5.jpg')}}" alt="Bandara Soekarno-Hatta - Tangerang" class="w-full">
                 <p class="font-bold">Bandara Soekarno-Hatta - Tangerang</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/6.jpg')}}" alt="Bandara Soekarno-Hatta - Tangerang" class="w-full">
                 <p class="font-bold">Bandara Soekarno-Hatta - Tangerang</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/7.jpg')}}" alt="Toilet Disabled Bandara Soekarno Hatta - Tangerang" class="w-full">
                 <p class="font-bold">Toilet Disabled Bandara Soekarno Hatta - Tangerang</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/8.jpg')}}" alt="Puri Indah Mall - Jakarta" class="w-full">
                 <p class="font-bold">Puri Indah Mall - Jakarta</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/9.jpg')}}" alt="Apartment St. Moritz - Jakarta" class="w-full">
                 <p class="font-bold">Apartment St. Moritz - Jakarta</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/10.jpg')}}" alt="Office St. Moritz - Jakarta" class="w-full">
                 <p class="font-bold">Office St. Moritz - Jakarta</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/11.jpg')}}" alt="Hotel La Lisa - Surabaya" class="w-full">
                 <p class="font-bold">Hotel La Lisa - Surabaya</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/12.jpg')}}" alt="Hotel Horison - Kertajati" class="w-full">
                 <p class="font-bold">Hotel Horison - Kertajati</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/13.jpg')}}" alt="RSIA Brawijaya - Jakarta" class="w-full">
                 <p class="font-bold">RSIA Brawijaya - Jakarta</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/14.jpg')}}" alt="RS Santo Borromeus - Bandung" class="w-full">
                 <p class="font-bold">RS Santo Borromeus - Bandung</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/15.jpg')}}" alt="RS Santo Borromeus - Bandung" class="w-full">
                 <p class="font-bold">RS Santo Borromeus - Bandung</p>
-                <p class="-mt-3">Hermetic Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Hermetic Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/16.jpg')}}" alt="RS Siloam Mampang - Jakarta" class="w-full">
                 <p class="font-bold">RS Siloam Mampang - Jakarta</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/17.jpg')}}" alt="RS Siloam TB Simatupang - Jakarta" class="w-full">
                 <p class="font-bold">RS Siloam TB Simatupang - Jakarta</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/18.jpg')}}" alt="RS EMC Pekayon - Bekasi" class="w-full">
                 <p class="font-bold">RS EMC Pekayon - Bekasi</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/19.jpg')}}" alt="RS EMC Pekayon - Bekasi" class="w-full">
                 <p class="font-bold">RS EMC Pekayon - Bekasi</p>
-                <p class="-mt-3">Double Swing Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Swing Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/20.jpg')}}" alt="RS EMC Pekayon - Bekasi" class="w-full">
                 <p class="font-bold">RS EMC Pekayon - Bekasi</p>
-                <p class="-mt-3">Hermetic Single Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Hermetic Single Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/21.jpg')}}" alt="Gedung Bintang Toedjoe - Bekasi" class="w-full">
                 <p class="font-bold">Gedung Bintang Toedjoe - Bekasi</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/22.jpg')}}" alt="MaxxBox Lippo Village - Tangerang" class="w-full">
                 <p class="font-bold">MaxxBox Lippo Village - Tangerang</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/23.jpg')}}" alt="Honda Daya Motor - Bandung" class="w-full">
                 <p class="font-bold">Honda Daya Motor - Bandung</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/24.jpg')}}" alt="Galaxy Mall - Surabaya" class="w-full">
                 <p class="font-bold">Galaxy Mall - Surabaya</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/25.jpg')}}" alt="Apartment Nine Residence - Jakarta" class="w-full">
                 <p class="font-bold">Apartment Nine Residence - Jakarta</p>
-                <p class="-mt-3">Double Sliding Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Sliding Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/26.jpg')}}" alt="Bank Indonesia - Yogyakarta" class="w-full">
                 <p class="font-bold">Bank Indonesia - Yogyakarta</p>
-                <p class="-mt-3">Double Swing Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Swing Door</p>
             </div>
 
-            <div class="flex flex-col gap-4 items-center col-span-1">
+            <div class="flex flex-col gap-3 sm:gap-3.5 md:gap-4 items-center col-span-1">
                 <img src="{{asset('assets/project/27.jpg')}}" alt="Bank Indonesia - Bandung" class="w-full">
                 <p class="font-bold">Bank Indonesia - Bandung</p>
-                <p class="-mt-3">Double Swing Door</p>
+                <p class="-mt-2 sm:-mt-2.5 md:-mt-3">Double Swing Door</p>
             </div>
         </div>
 
-        <div class="bg-custom-lighter-blue px-8 pt-6 pb-3 relative mt-16">
-            <h1 class="text-custom-dark-blue font-ttRamillas font-extrabold absolute -top-5 text-2xl sm:text-3xl md:text-4xl">Project Reference</h1>
+        <div class="bg-custom-lighter-blue px-6 sm:px-7 md:px-8 pt-4 sm:pt-5 md:pt-6 pb-2 sm:pb-2.5 md:pb-3 relative mt-16">
+            <h1 class="text-custom-dark-blue font-ttRamillas font-extrabold absolute -top-4 sm:-top-[1.125rem] md:-top-5 text-2xl sm:text-3xl md:text-4xl">Project Reference</h1>
             <p class="text-custom-dark-blue/90">Daftar pelanggan dan proyek yang telah kami selesaikan</p>
         </div>
 
-        <div class="px-12 sm:px16 md:px-20 lg:px-24 xl:px-28 2xl:px-32 bg-custom-dark-blue flex flex-col items-center rounded-3xl p-8 text-custom-white gap-6">
+        <div class="px-10 sm:px-16 md:px-20 lg:px-24 xl:px-28 2xl:px-32 bg-custom-dark-blue flex flex-col items-center rounded-3xl py-8 text-custom-white gap-6">
             <h1 class="font-ttRamillas font-extrabold text-base sm:text-lg md:text-xl">Downloads</h1>
 
             <div class="border-b border-b-custom-white flex justify-between w-full pb-2 sm:pb-3 md:pb-4 gap-4">
