ea1be0c67bce916c7863eafce8f35d395a530dfc
TessyJr <christopher.nathanael1217@gmail.com>
2023-10-12 14:50:24 +0700

    style: fix product styling

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -15,29 +15,29 @@
     <div class="c-container text-paragraph py-16 sm:py-24 md:py-32 flex flex-col gap-8 justify-center items-center">
         <h1 class="text-custom-dark-blue font-ttRamillas font-extrabold text-2xl sm:text-3xl md:text-4xl text-center">Product Category</h1>
 
-        <div class="grid grid-cols-2 lg:grid-cols-4 w-full gap-4 text-custom-darker-blue">
-            <a href="" class="flex flex-col gap-4 overflow-hidden">
+        <div class="grid grid-cols-2 lg:grid-cols-4 w-full gap-4 sm:gap-5 md:gap-6 text-custom-darker-blue">
+            <a href="" class="flex flex-col gap-2.5 sm:gap-3 md:gap-4 overflow-hidden">
                 <div class="h-[300px] sm:[350px] md:h-[400px] lg:h-[450px] xl:h-[500px] overflow-hidden">
                     <div class="bg-cover bg-center h-full col-span-1 hover:scale-125 transition" style="background-image: url({{asset('assets/product/automatic-sliding-door.jpg')}});"></div>
                 </div>
                 <p class="font-ttRamillas font-extrabold text-center">Automatic Sliding Door</p>
             </a>
 
-            <a href="" class="flex flex-col gap-4 overflow-hidden">
+            <a href="" class="flex flex-col gap-2.5 sm:gap-3 md:gap-4 overflow-hidden">
                 <div class="h-[300px] sm:[350px] md:h-[400px] lg:h-[450px] xl:h-[500px] overflow-hidden">
                     <div class="bg-cover bg-center h-full col-span-1 hover:scale-125 transition" style="background-image: url({{asset('assets/product/automatic-swing-door.jpg')}});"></div>
                 </div>
                 <p class="font-ttRamillas font-extrabold text-center">Automatic Swing Door</p>
             </a>
 
-            <a href="" class="flex flex-col gap-4 overflow-hidden">
+            <a href="" class="flex flex-col gap-2.5 sm:gap-3 md:gap-4 overflow-hidden">
                 <div class="h-[300px] sm:[350px] md:h-[400px] lg:h-[450px] xl:h-[500px] overflow-hidden">
                     <div class="bg-cover bg-center h-full col-span-1 hover:scale-125 transition" style="background-image: url({{asset('assets/product/automatic-revolving-door.jpg')}});"></div>
                 </div>
                 <p class="font-ttRamillas font-extrabold text-center">Automatic Revolving Door</p>
             </a>
 
-            <a href="" class="flex flex-col gap-4 overflow-hidden">
+            <a href="" class="flex flex-col gap-2.5 sm:gap-3 md:gap-4 overflow-hidden">
                 <div class="h-[300px] sm:[350px] md:h-[400px] lg:h-[450px] xl:h-[500px] overflow-hidden">
                     <div class="bg-cover bg-center h-full col-span-1 hover:scale-125 transition" style="background-image: url({{asset('assets/product/hermetic-door.jpg')}});"></div>
                 </div>
