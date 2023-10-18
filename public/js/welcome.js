0e9f141cf40db5ee94a19a7cfef77ae5d0acf6ca
TessyJr <christopher.nathanael1217@gmail.com>
2023-10-18 09:31:54 +0700

    add: add carousel to home

---
+++ public/js/welcome.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,103 @@
 @extends('template.template-en')
 
 @section('head')
-    {{-- css --}}
-    {{-- <link rel="stylesheet" href="{{ asset('css/[name].css') }}?t={{ env('VERSION_TIME') }}"> --}}
+{{-- css --}}
+{{-- <link rel="stylesheet" href="{{ asset('css/[name].css') }}?t={{ env('VERSION_TIME') }}"> --}}
+
+{{-- Swiper JS --}}
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+
+<style>
+    .swiper {
+      width: 240px;
+      height: 320px;
+    }
+
+    .swiper-slide {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 18px;
+      font-size: 22px;
+      font-weight: bold;
+      color: #fff;
+    }
+
+    .swiper-slide:nth-child(1n) {
+      background-color: rgb(206, 17, 17);
+    }
+
+    .swiper-slide:nth-child(2n) {
+      background-color: rgb(0, 140, 255);
+    }
+
+    .swiper-slide:nth-child(3n) {
+      background-color: rgb(10, 184, 111);
+    }
+
+    .swiper-slide:nth-child(4n) {
+      background-color: rgb(211, 122, 7);
+    }
+
+    .swiper-slide:nth-child(5n) {
+      background-color: rgb(118, 163, 12);
+    }
+
+    .swiper-slide:nth-child(6n) {
+      background-color: rgb(180, 10, 47);
+    }
+
+    .swiper-slide:nth-child(7n) {
+      background-color: rgb(35, 99, 19);
+    }
+
+    .swiper-slide:nth-child(8n) {
+      background-color: rgb(0, 68, 255);
+    }
+
+    .swiper-slide:nth-child(9n) {
+      background-color: rgb(218, 12, 218);
+    }
+
+    .swiper-slide:nth-child(10n) {
+      background-color: rgb(54, 94, 77);
+    }
+</style>
 @endsection
 
 @section('body')
-    <x-navbar-en :page="'home'" />
+<x-navbar-en :page="'home'" />
 
-    <div class="c-container py-32 min-h-screen">
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+<div class="c-container py-32 min-h-screen">
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+
+
+    <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">Slide 1</div>
+            <div class="swiper-slide">Slide 2</div>
+            <div class="swiper-slide">Slide 3</div>
+            <div class="swiper-slide">Slide 4</div>
+            <div class="swiper-slide">Slide 5</div>
+            <div class="swiper-slide">Slide 6</div>
+            <div class="swiper-slide">Slide 7</div>
+            <div class="swiper-slide">Slide 8</div>
+            <div class="swiper-slide">Slide 9</div>
+        </div>
+
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
     </div>
+</div>
 
-    <x-footer />
+<x-footer />
+
+{{-- Swiper JS --}}
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+{{-- JS --}}
+<script src="{{ asset('js/welcome.js') }}?t={{ env('VERSION_TIME') }}"></script>
 @endsection
