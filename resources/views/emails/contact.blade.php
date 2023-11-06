23debc6f77b905db8d0cd1a5fea50724c03d7226
TessyJr <christopher.nathanael1217@gmail.com>
2023-11-06 15:28:20 +0700

    style: fix email and website styling

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -4,15 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>PT Dutacitra Nusa Jaya</title>
 </head>
 <body>
-    <h1>Pesan dari Formulir Kontak</h1>
+    <h1>Contact Form Message</h1>
+    <p>From <a href="">PT Dutacitra Nusa Jaya Website</a></p>
+    <br>
+    <div>
+        <h2>First Name:</h2>
+        <p>{{$firstName}}</p>
+    </div>
+    <hr>
+    <div>
+        <h2>Last Name:</h2>
+        <p>{{$lastName}}</p>
+    </div>
+    <hr>
+    <div>
+        <h2>Email:</h2>
+        <a href="mailto:{{ $email }}">{{ $email }}</a>
+    </div>
+    <hr>
+    <div>
+        <h2>Phone:</h2>
+        <p>{{$phone}}</p>
+    </div>
+    <hr>
+    <div>
+        <h2>Subject:</h2>
+        <p>{{$subject}}</p>
+    </div>
+    <hr>
+    <div>
+        <h2>Message:</h2>
+        <p>{{$contactMessage}}</p>
+    </div>
 
-    <p>Email Pengirim: </p>
-    <a href="mailto:{{ $email }}">{{ $email }}</a>
-
-    <p>Pesan: {{ $contactMessage }}</p>
 </body>
 </html>
-
