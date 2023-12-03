07c24ccd177c24aca7ddbc9d047f530477b36ac2
TessyJr <christopher.nathanael1217@gmail.com>
2023-12-03 19:54:23 +0700

    fix: fix icons

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -19,8 +19,8 @@
 
     {{-- Favicon --}}
     <link rel="icon" href="/favicon.ico?t={{ env('VERSION_TIME') }}" sizes="48x48">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?t={{ env('VERSION_TIME') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?t={{ env('VERSION_TIME') }}">
 
     {{-- Apple Touch Icon --}}
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?t={{ env('VERSION_TIME') }}">
